ae72f85ee870a00d06bdc503eeaf2fec1ce6eaf0
Joshua <joshua6005@gmail.com>
2020-11-30 08:32:25 -0600

    Reemoved default login to merge Manoj's changes

--- app/src/main/java/com/example/livephms/Communication.java
+++ app/src/main/java/com/example/livephms/Communication.java
@@ -1,15 +1,49 @@
 package com.example.livephms;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.core.app.NotificationCompat;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.telephony.SmsManager;
 import android.widget.Toast;
 import java.util.ArrayList;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 
 public class Communication extends AppCompatActivity {
     public void pushNotification(String message) {
+        /*// Prepare Notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle("TITLE")
+                .setContentText(message)
+                .setContentIntent(contentIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // For API 26 and above
+            CharSequence channelName = "My Notification";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, channelName, importance);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        // NotificationManager
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // Notify
+        notificationManager.notify(notificationId, builder.build());*/
     }
 
     public void textSMS(String message) {
